1cbe7a9979f51e797d7ec99f8ece39f4e85286cb
Exil_S <112499822+xXExilXx@users.noreply.github.com>
2023-09-27 21:19:38 +0200

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using UnityEngine;
+
+
+namespace MediaControlls
+{
+    public static class BuildInfo
+    {
+        public const string Name = "MediaControlls"; // Name of the Mod.  (MUST BE SET)
+        public const string Description = "Let's you controll your pc music from UI"; // Description for the Mod.  (Set as null if none)
+        public const string Author = "Exil_S"; // Author of the Mod.  (MUST BE SET)
+        public const string Company = null; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+    }
+
+    public class MediaControlls : MelonMod
+    {
+        private const int KEYEVENTF_EXTENDEDKEY = 1;
+        private const int KEYEVENTF_KEYUP = 0;
+        private const int VK_MEDIA_NEXT_TRACK = 0xB0;
+        private const int VK_MEDIA_PLAY_PAUSE = 0xB3;
+        private const int VK_MEDIA_PREV_TRACK = 0xB1;
+
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(byte virtualKey, byte scanCode, uint flags, IntPtr extraInfo);
+        private bool isMenuVisible = false;
+
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.F5))
+            {
+                isMenuVisible = !isMenuVisible;
+            }
+        }
+
+        public override void OnGUI()
+        {
+            if (isMenuVisible)
+            {
+
+                if (GUI.Button(new Rect(10, 10, 100, 50), "Play/Pause"))
+                {
+                    TogglePlayback();
+                }
+
+                if (GUI.Button(new Rect(10, 70, 100, 50), "Skip Forward"))
+                {
+                    SkipForward();
+                }
+
+                if (GUI.Button(new Rect(10, 130, 100, 50), "Skip Back"))
+                {
+                    SkipBack();
+                }
+            }
+        }
+
+        private void SimulateMediaKey(byte virtualKey)
+        {
+            keybd_event(virtualKey, 0, KEYEVENTF_EXTENDEDKEY, IntPtr.Zero);
+            keybd_event(virtualKey, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, IntPtr.Zero);
+        }
+
+        void TogglePlayback()
+        {
+            SimulateMediaKey(VK_MEDIA_PLAY_PAUSE);
+        }
+
+        void SkipForward()
+        {
+            SimulateMediaKey(VK_MEDIA_NEXT_TRACK);
+        }
+
+        void SkipBack()
+        {
+            SimulateMediaKey(VK_MEDIA_PREV_TRACK);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using MelonLoader;
+
+[assembly: AssemblyTitle(MediaControlls.BuildInfo.Description)]
+[assembly: AssemblyDescription(MediaControlls.BuildInfo.Description)]
+[assembly: AssemblyCompany(MediaControlls.BuildInfo.Company)]
+[assembly: AssemblyProduct(MediaControlls.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + MediaControlls.BuildInfo.Author)]
+[assembly: AssemblyTrademark(MediaControlls.BuildInfo.Company)]
+[assembly: AssemblyVersion(MediaControlls.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(MediaControlls.BuildInfo.Version)]
+[assembly: MelonInfo(typeof(MediaControlls.MediaControlls), MediaControlls.BuildInfo.Name, MediaControlls.BuildInfo.Version, MediaControlls.BuildInfo.Author, MediaControlls.BuildInfo.DownloadLink)]
+[assembly: MelonColor()]
+
+// Create and Setup a MelonGame Attribute to mark a Melon as Universal or Compatible with specific Games.
+// If no MelonGame Attribute is found or any of the Values for any MelonGame Attribute on the Melon is null or empty it will be assumed the Melon is Universal.
+// Values for MelonGame Attribute can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame(null, null)]
